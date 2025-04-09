c5e3120e0e0a674b88931ca0e1b397a50f9352d6
Minh Minh <168627170+MinhPhanNguyen@users.noreply.github.com>
2025-04-09 18:00:56 +0700

    Delete NikeStore directory

--- NikeStore/NikeStore/Models/AppUserModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace NikeStore.Models
-{
-    public class AppUserModel : IdentityUser
-    {
-        public string RoleId { get; set; }
-    }
-}
--- NikeStore/NikeStore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NikeStore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
